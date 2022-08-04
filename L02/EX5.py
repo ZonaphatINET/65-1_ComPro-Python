02ba456fab2955ae5798da383a58d258aae73d1a
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2022-08-05 04:16:25 +0700

    updeta

--- L02/EX5.py
+++ L02/EX5.py
@@ -2,8 +2,8 @@ time_wrok = float (input('Enter the number of hours worke: '))
 pay_rate = float (input('Enter the hourly pay reat: '))
 if time_wrok <= 40:
     gross_pay = time_wrok * pay_rate
-    print('The gross pay is $%.2f'(gross_pay))
+    print('The gross pay is $%.2f.'%(gross_pay))
 else :
-    over_time = time_wrok - 40
-    gross_pay = (time_wrok * pay_rate)+(over_time * ((pay_rate  / 100) * 150))
-    print('The gross pay is $%.2f'(gross_pay))
+    over_time = (time_wrok - 40)
+    gross_pay = (40 * pay_rate) + (over_time * ((pay_rate / 100)* 150))
+    print('The gross pay is $%.2f.'%(gross_pay))
