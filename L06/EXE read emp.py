cfa1cf33ac2f323bbec6869abecf9633c33245e5
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2022-08-30 22:19:18 +0700

    updeta

--- L06/EXE read emp.py
+++ L06/EXE read emp.py
@@ -1,9 +1,26 @@
 def main():
     sales_file = open('employees.txt','r')
-
+    i = 1
+    j = 2
+    k = 3
+    l = 3
+    m = 1
     for line in sales_file:
         amount = str(line)
-        print(format(amount))
+        if i == m:
+            print('Name: ',amount.rstrip('\n'))
+            i += l
+            m += 1
+        elif j == m:
+            print('ID: ',amount.rstrip('\n'))
+            j += l 
+            m += 1
+        elif k == m:
+            print('Dept: ',amount.rstrip('\n'))
+            k += l 
+            m += 1
+        else:
+            print()
 
     sales_file.close
 
