8c46ffe1ec8af7b41eb5acf4b0840c9b5c961191
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2022-09-22 00:44:52 +0700

    updeta

--- L07/test input list.py
+++ L07/test input list.py
@@ -1,14 +1,36 @@
 moreHeroes = int(input('Enter number your heroes : '))
+heroes = [0] * moreHeroes
+for index in range(moreHeroes):
+    print('Enter name heroes ',index +1 ,' : ',sep='',end='')
+    heroes[index] = str(input())
 
 def inputHeroes():
-    heroes = [0] * moreHeroes
-
-    for index in range(moreHeroes):
-        print('Enter name heroes ',index +1 ,' : ',sep='',end='')
-        heroes[index] = str(input())
-
     print(heroes)
 
-inputHeroes()
+def insert():
+    Hero = input('Enter name to add : ')
+    index = int(input('Enter location name : '))
+    heroes.insert(index,Hero)
+    inputHeroes()
 
+def remove():
+    Hero = input('Enter name to remove : ')
+    heroes.remove(Hero)
+    inputHeroes()
 
+def Dissort():
+    heroes.sort()
+    inputHeroes()
+
+def reverse():
+    heroes.reverse()
+    print(heroes)
+
+def main():
+    inputHeroes()
+    insert()
+    remove()
+    Dissort()
+    reverse()
+
+main()
