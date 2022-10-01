62c2c38a59244403a7a220911ec39a90feea2f94
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2022-10-01 16:43:28 +0700

    update

--- HW/StrHW.py
+++ HW/StrHW.py
@@ -2,16 +2,16 @@ strg = str(input('Input String: '))
 vow = 'AaEeIiOoUu'
 
 def OnlyStrg():
-    strg.replace(' ','')
+
     print('Number of Letters:' , len(strg.replace(' ','')))
 
 def Vowels():
-    final = [each for each in strg if each in vow]
-    print('Number of vowels:',len(final))
+    numVow = [masg for masg in strg if masg in vow]
+    print('Number of vowels:',len(numVow))
 
 def Capital():
-    res = [char for char in strg if char.isupper()]
-    print('Number of Capital:',len(res),res)
+    res = [upper for upper in strg if upper.isupper()]
+    print('Number of Capital: %d, '%len(res)+'[%s]'%', '.join(map(str,res)))
 
 def main():
     OnlyStrg()
