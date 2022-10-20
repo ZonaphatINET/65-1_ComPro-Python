f89297d4c55a07d0e2fa6e5016e0c0eeb9165539
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2022-10-20 11:36:27 +0700

    update

--- L09/TypeLetter.py
+++ L09/TypeLetter.py
@@ -17,8 +17,8 @@ def draw():  # Pygame Zero draw function
     screen.fill(BLACK)
     for LETTER in ON_SCREEN_LETTERS:
         screen.draw.text(LETTER["letter"], (LETTER["x"], LETTER["y"]), fontsize=50, color=WHITE)
-    screen.draw.text("RIGHT: " + str(SCORE["RIGHT"]), (WIDTH - 130, 10), fontsize=30, color=WHITE)
-    screen.draw.text("WRONG: " + str(SCORE["WRONG"]), (WIDTH - 130, 40), fontsize=30, color=WHITE)
+        screen.draw.text("RIGHT: " + str(SCORE["RIGHT"]), (WIDTH - 130, 10), fontsize=30, color=WHITE)
+        screen.draw.text("WRONG: " + str(SCORE["WRONG"]), (WIDTH - 130, 40), fontsize=30, color=WHITE)
 
 
 def update():
