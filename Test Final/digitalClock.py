2addb314de4663936226e24374ad7e552741b501
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2022-10-25 15:45:52 +0700

    update

--- Test Final/digitalClock.py
+++ Test Final/digitalClock.py
@@ -1,6 +1,7 @@
 def digitalClock (i):
     T1 = 0
     T2 = 0
+    T3 = 0
     time = 60
     sets = 60
     for h in range(0,i):
@@ -9,3 +10,6 @@ def digitalClock (i):
             time += 60
         elif T2 == sets:
             T1 + 1
+
+
+print(digitalClock())
